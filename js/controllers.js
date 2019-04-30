b4955ea5a65debf97e7027edd5a7551923a533ee
Gustavo92br <50118548+Gustavo92br@users.noreply.github.com>
2019-04-30 15:59:43 -0300

    Initial commit

---
+++ js/controllers.js
@@ -0,0 +1,4 @@
+angular.module('starter')
+.controller('HomeController', function($scope){
+
+});
---
+++ js/routes.js
@@ -0,0 +1,10 @@
+angular .module('starter')
+.config(function($stateProvider, $urlRouterProvider){
+	$urlRouterProvider.otherwise('/home');
+	$stateProvider
+	.state('home', {
+		url: '/home',
+		templateUrl: 'templates/home.html',
+		controller: 'HomeController'
+	})
+});
